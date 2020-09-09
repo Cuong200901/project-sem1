80c947681adce3b45f8cf2661822b3c45e66dc11
hoang cuong <hoangcuong20092001@gmail.com>
2020-09-09 22:01:12 +0700

    .

--- lta/src/main/java/vtc/App.java
+++ lta/src/main/java/vtc/App.java
@@ -1,29 +1,18 @@
 package vtc;
 
+import java.io.IOException;
 
 import vtc.ui.AccountUI.AccountFunctionUI;
 
-import java.io.IOException;
 
-import java.sql.*;
 
 public class App {
 
-    public static void main(final String[] args) throws SQLException {
-
+    public static void main(String[] args) {
         cls();
-           String strconn =
-        "jdbc:mysql://localhost:3306/lemon_tee_shop?user=root&password=cuong2001";
-        try ( Connection conn = DriverManager.getConnection(strconn);)
-        {
-        System.out.println("Connected to MySql Server.");
-        } catch (SQLException ex) {
-        System.out.println("Connection Error!");
-        System.out.println(ex);
-        }
+      
         AccountFunctionUI.login();
     }
-
     public static void cls() {
         try {
             if (System.getProperty("os.name").contains("Windows"))
